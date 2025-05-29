cee2a396460998c0335469dc1290ce2719c99e73
Pratik Shendge <pratikshendge79@gmail.com>
2025-05-29 06:21:39 +0000

    Model and dbt_project.yml file is updated

---
+++ models/customers/stg_customers.sql
@@ -0,0 +1,15 @@
+with source as (
+    select
+        CUSTOMER_ID,
+        trim(FIRST_NAME) as first_name,
+        trim(LAST_NAME) as last_name,
+        lower(trim(EMAIL)) as email,
+        try_cast(BIRTHDATE as date) as birthdate,
+        ADDRESS,
+        cast(CC as string) as credit_card,
+        CC_TYPE,
+        UPDATED_AT
+    from {{ source('SC_DBPRACT', 'CUSTOMER') }}
+)
+
+select * from source

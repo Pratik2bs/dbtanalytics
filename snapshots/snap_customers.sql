b8d7904c29f75c2396c7a8d22f10d644fad9a032
Pratik Shendge <pratikshendge79@gmail.com>
2025-05-29 09:51:42 +0000

    dd

---
+++ snapshots/snap_customers.sql
@@ -0,0 +1,23 @@
+{% snapshot customer_snapshot %}
+{{
+    config(
+        target_schema='SC_DBPRACT',
+        unique_key='CUSTOMER_ID',
+        strategy='timestamp',
+        updated_at='UPDATED_AT'
+    )
+}}
+
+select
+    CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    EMAIL,
+    BIRTHDATE,
+    ADDRESS,
+    CC,
+    CC_TYPE,
+    UPDATED_AT
+from {{ source('mysource', 'CUSTOMER') }}
+
+{% endsnapshot %}
